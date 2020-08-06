955e60389cb28d58d483d03e801be1ebcf21b72e
John Lopez <johncarlos1943@gmail.com>
2020-08-06 15:00:51 -0400

    creacion de clase medida y buscarPac.html

--- src/main/java/logico/goniometriaClass/Medida.java
+++ src/main/java/logico/goniometriaClass/Medida.java
@@ -1,7 +1,119 @@
 package logico.goniometriaClass;
 
 public class Medida {
-    protected String id_medida;
-    protected String id_especialista;
-    protected String fecha_realizacion;
+    private String id_medida;
+    private String id_especialista;
+    private String fecha_realizacion;
+    private String medida;
+    private String lugar;
+    private String id_angulo;
+    private String id_articulaciones;
+    private String id_cuatrodedos;
+    private String rom;
+    private String id_rom;
+    private double rom_value;
+
+    public Medida() {
+    }
+
+    public Medida(String id_medida, String id_especialista, String fecha_realizacion, String medida, String lugar, String id_angulo, String id_articulaciones, String rom, String id_rom, double rom_value) {
+        this.id_medida = id_medida;
+        this.id_especialista = id_especialista;
+        this.fecha_realizacion = fecha_realizacion;
+        this.medida = medida;
+        this.lugar = lugar;
+        this.id_angulo = id_angulo;
+        this.id_articulaciones = id_articulaciones;
+        this.rom = rom;
+        this.id_rom = id_rom;
+        this.rom_value = rom_value;
+    }
+
+    public String getId_medida() {
+        return id_medida;
+    }
+
+    public void setId_medida(String id_medida) {
+        this.id_medida = id_medida;
+    }
+
+    public String getId_especialista() {
+        return id_especialista;
+    }
+
+    public void setId_especialista(String id_especialista) {
+        this.id_especialista = id_especialista;
+    }
+
+    public String getFecha_realizacion() {
+        return fecha_realizacion;
+    }
+
+    public void setFecha_realizacion(String fecha_realizacion) {
+        this.fecha_realizacion = fecha_realizacion;
+    }
+
+    public String getMedida() {
+        return medida;
+    }
+
+    public void setMedida(String medida) {
+        this.medida = medida;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public String getId_angulo() {
+        return id_angulo;
+    }
+
+    public void setId_angulo(String id_angulo) {
+        this.id_angulo = id_angulo;
+    }
+
+    public String getId_articulaciones() {
+        return id_articulaciones;
+    }
+
+    public void setId_articulaciones(String id_articulaciones) {
+        this.id_articulaciones = id_articulaciones;
+    }
+
+    public String getId_cuatrodedos() {
+        return id_cuatrodedos;
+    }
+
+    public void setId_cuatrodedos(String id_cuatrodedos) {
+        this.id_cuatrodedos = id_cuatrodedos;
+    }
+
+    public String getRom() {
+        return rom;
+    }
+
+    public void setRom(String rom) {
+        this.rom = rom;
+    }
+
+    public String getId_rom() {
+        return id_rom;
+    }
+
+    public void setId_rom(String id_rom) {
+        this.id_rom = id_rom;
+    }
+
+    public double getRom_value() {
+        return rom_value;
+    }
+
+    public void setRom_value(double rom_value) {
+        this.rom_value = rom_value;
+    }
 }
---
+++ src/main/java/logico/servicios/MedidaServicios.java
@@ -0,0 +1,185 @@
+package logico.servicios;
+
+import logico.goniometriaClass.Goniometria;
+import logico.goniometriaClass.Medida;
+
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MedidaServicios {
+    private static MedidaServicios instancia;
+
+    public static MedidaServicios getInstance( ) {
+        if (instancia == null)
+            instancia = new MedidaServicios();
+        return instancia;
+    }
+
+    public List<Medida> listaEstudiantes() {
+        List<Medida> lista = new ArrayList<>();
+        /*Connection con = null; //objeto conexion.
+        try {
+            //
+            String query = "select * from estudiante ";
+            con = DataBaseServices.getInstancia().getConexion(); //referencia a la conexion.
+            //
+            PreparedStatement prepareStatement = con.prepareStatement(query);
+            ResultSet rs = prepareStatement.executeQuery();
+            while(rs.next()){
+                Medida est = new Medida();
+                est.setMatricula(rs.getInt("matricula"));
+                est.setNombre(rs.getString("nombre"));
+                est.setApellido(rs.getString("apellido"));
+                est.setCarrera(rs.getString("carrera"));
+                est.setTelefono(rs.getString("telefono"));
+
+                lista.add(est);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(EstudianteServices.class.getName()).log(Level.SEVERE, null, ex);
+        } finally{
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(EstudianteServices.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }*/
+
+        return lista;
+    }
+    public Medida getMedida(String id_Medida, String lug) {
+        Medida est = null;
+        Connection con = null;
+        try {
+            //utilizando los comodines (?)...
+
+            String query = "select t.ID_paciente, t.ID_medida, m.Fecha_realizacion, m.ID_especialista, a.ID_angulo, ac.ID_cuatrodedos, cu.id_dedos, r.ID_rom,\n" +
+                    "       ac.Interfalangicas_distales , ac.Interfalangicas_proximales , ac.Metacarpofalangicas, cu.anular,\n" +
+                    "        cu.indice, cu.mayor, cu.menique, r.Flexion, r.Extension, r.Abduccion, r.Aduccion\n" +
+                    "\n" +
+                    "from medidas m, angulos a, cuatrodedos cu, articulaciones_cuatrodedos ac, rom r, terapia t\n" +
+                    "where m.ID_medida = a.ID_medida and a.ID_cuatrodedos = ac.ID_cuatrodedos and ac.ID_dedos = cu.id_dedos\n" +
+                    "and cu.ID_rom = r.ID_rom and t.ID_medida = m.ID_medida and t.ID_medida = ?;";
+            con = DataBaseServices.getInstancia().getConexion();
+            //
+            PreparedStatement prepareStatement = con.prepareStatement(query);
+            //Antes de ejecutar seteo los parametros.
+            prepareStatement.setString(1, id_Medida);
+            //Ejecuto...
+            ResultSet rs = prepareStatement.executeQuery();
+            while(rs.next()){
+                est = new Medida();
+                est.setId_medida(rs.getString(2));
+                Date date = rs.getDate(3);
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+                String strDate = dateFormat.format(date);
+                est.setFecha_realizacion(strDate);
+                est.setId_especialista(rs.getString(4));
+                est.setId_angulo(rs.getString(5));
+                if(rs.getString(6) != null){
+                    est.setId_articulaciones(rs.getString(6));
+                    est.setId_cuatrodedos(rs.getString(7));
+                    est.setId_rom(rs.getString(8));
+                    if(rs.getInt(9) == 1){
+                        est.setMedida(selectMedida(4));
+                    }else if(rs.getInt(10) == 1){
+                        est.setMedida(selectMedida(3));
+                    }else if(rs.getInt(11) == 1){
+                        est.setMedida(selectMedida(2));
+                    }
+                    if(rs.getInt(12) == 1){
+                        est.setLugar(selectdedo(3));
+                    }else if(rs.getInt(13) == 1){
+                        est.setLugar(selectdedo(1));
+                    }else if(rs.getInt(14) == 1){
+                        est.setLugar(selectdedo(2));
+                    }else if(rs.getInt(15) == 1){
+                        est.setLugar(selectdedo(4));
+                    }
+                    if(rs.getDouble(16) != -1){
+
+                        est.setRom(selectRom(1));
+                        est.setRom_value(rs.getDouble(16));
+                    }else if(rs.getDouble(17) != -1){
+                        est.setRom(selectRom(2));
+                        est.setRom_value(rs.getDouble(17));
+                    }else if(rs.getDouble(18) != -1){
+                        est.setRom(selectRom(3));
+                        est.setRom_value(rs.getDouble(18));
+                    }else if(rs.getDouble(19) != -1){
+                        est.setRom(selectRom(4));
+                        est.setRom_value(rs.getDouble(19));
+                    }
+                }
+
+
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(MedidaServicios.class.getName()).log(Level.SEVERE, null, ex);
+        } finally{
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(MedidaServicios.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        return est;
+    }
+    private String selectRom(int index){
+        switch (index){
+            case 1:
+                return "Flexion";
+            case 2:
+                return "Extension";
+            case 3:
+                return "abduccion";
+            case 4:
+                return "aduccion";
+        }
+        return null;
+    }
+
+
+    private String selectdedo(int index){
+        switch (index){
+            case 1:
+                return "indice";
+            case 2:
+                return "medio";
+            case 3:
+                return "anular";
+            case 4:
+                return "meñique";
+            case 5:
+                return "pulgar";
+        }
+        return null;
+    }
+
+    private String selectMedida(int index){
+        switch (index){
+            case 1:
+                return "carpometacarpiana";
+            case 2:
+                return "metocarpofalangicas";
+            case 3:
+                return "interfalangicas proximales";
+            case 4:
+                return "interfalangicas Distales";
+        }
+        return null;
+    }
+
+
+
+
+
+}
