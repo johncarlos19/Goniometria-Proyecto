dee747c7c727e912910f87d79e7ded7d497dca8b
John Lopez <johncarlos1943@gmail.com>
2020-08-06 10:55:17 -0400

    creacion de clase medida y buscarPac.html

---
+++ src/main/java/logico/goniometriaClass/Medida.java
@@ -0,0 +1,7 @@
+package logico.goniometriaClass;
+
+public class Medida {
+    protected String id_medida;
+    protected String id_especialista;
+    protected String fecha_realizacion;
+}
---
+++ src/main/java/logico/servicios/Medidaservicios.java
@@ -0,0 +1,4 @@
+package logico.servicios;
+
+public class Medidaservicios {
+}
